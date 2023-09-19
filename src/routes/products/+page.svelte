caa03df546f76170cdcbf86082865e9a96c4bb45
BrunoFG <b.frontiniganem@bssweb.it>
2023-09-19 16:34:49 +0200

    fixing the grid

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -14,11 +14,16 @@
 	<Row>
 		{#each products as product}
 			<Column sm={3.5} md={4} lg={5}>
-				<Tile>
+				<Tile style="border-radius: 3%; margin-top: 4%;">
 					<h1>{product.title}</h1>
-					<p>{product.description}</p>
-					<ImageLoader src={product.thumbnail} alt={product.description} ratio="1x1" />
-					<Button>$ {product.price}</Button>
+					<ImageLoader
+						src={product.thumbnail}
+						alt={product.description}
+						ratio="1x1"
+						style="border-radius: 3%; align-self: unset; object-fit: contain; max-height: 100%;"
+					/>
+					<p style="margin: 3%;">{product.description}</p>
+					<Button style="border-radius: 5%;">$ {product.price}</Button>
 				</Tile>
 			</Column>
 		{/each}
